9779bbefbee9e888c10e9e0ea548081d600f41b7
ffacon <francois.facon@gmail.com>
2015-03-23 11:43:01 +0100

    Update DetailController

--- src/main/webapp/bookcat/js/controllers/DetailController.js
+++ src/main/webapp/bookcat/js/controllers/DetailController.js
@@ -27,6 +27,42 @@
 		        }
 		        return 0;
 		    };
+		    
+		    $scope.getImage = function (id) {
+		        if (!id) {
+		            return "";
+		        } else {
+		            return "/bookcat/img/catalog/" + id + ".jpg";
+		        }
+		    };
+		    
+		    /** Returns the CSS class for the average rating of a given product. */
+		    $scope.getCSSRating = function(pItem) {
+		        var css = ['rating'];
+
+		        if (pItem !== undefined) {
+		            switch (getProductRating(pItem)) {
+		                case 1:
+		                    css.push('one');
+		                    break;
+		                case 2:
+		                    css.push('two');
+		                    break;
+		                case 3:
+		                    css.push('three');
+		                    break;
+		                case 4:
+		                    css.push('four');
+		                    break;
+		                case 5:
+		                    css.push('five');
+		                    break;
+		                default :
+		                    css.push('zero');
+		            }
+		        }
+		        return css;
+		    };
 
 
 }]);
