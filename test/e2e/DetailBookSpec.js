930f34a4d03c1765d028ea99e40075fc481dee28
ffacon <francois.facon@gmail.com>
2015-03-24 15:08:48 +0100

    Update DetailBookSpec e2e test

--- test/e2e/DetailBookSpec.js
+++ test/e2e/DetailBookSpec.js
@@ -4,4 +4,24 @@ describe("Detail Book Page", function () {
         browser.get('http://localhost:8080/bookcat#/book/1');
     });
 
+    it("Check Book Name", function () {
+        expect(element(by.binding('product.name')).getText()).toEqual('ANGULARJS');
+    });
+
+    it('The price change when updating quantity', function () {
+        expect(element(by.binding('product.price * quantity |currency')).getText()).toEqual('$15.34');
+        var qty = element(by.model('quantity'));
+        qty.clear();
+        /*browser.debugger();*/
+        qty.sendKeys(2);
+        expect(element(by.binding('product.price * quantity |currency')).getText()).toEqual('$30.68');
+    });
+
+
+
+    it('The image url is correct', function () {
+        browser.debugger();
+        expect(element(by.css('.photo')).getAttribute('src')).toEqual('http://localhost:8080/bookcat/img/catalog/1.jpg');
+    });
+
 });
