78f214fbd14fbe610087e624bedf2e6bd4d31cb6
ffacon <francois.facon@gmail.com>
2015-03-25 09:05:18 +0100

    Use $resource in  NewsService

--- src/main/webapp/bookcat/js/services/NewsService.js
+++ src/main/webapp/bookcat/js/services/NewsService.js
@@ -1,4 +1,10 @@
 (function(){
     "use strict";
-
+    angular.module('app')
+        .factory('NewsService',['$resource', function ($resource) {
+            return $resource('/api/app/news/:op/:id',{id:'@id'},{
+            like: {method:'GET',params: {op:'like'}},
+            random: {method:'GET',params:{op:'random'}}
+                });
+        }]);
 })();
