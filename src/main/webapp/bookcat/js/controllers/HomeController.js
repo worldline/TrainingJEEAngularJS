400f5769ffaf7e7c52dee681a2b3305240c8778e
ffacon <francois.facon@gmail.com>
2015-03-25 09:06:03 +0100

    Update HomeController in order to use NewsService

--- src/main/webapp/bookcat/js/controllers/HomeController.js
+++ src/main/webapp/bookcat/js/controllers/HomeController.js
@@ -3,9 +3,41 @@
 
     /** Home view controller */
     angular.module('app')
-        .controller('HomeController', ['$scope', function ($scope) {
+        .controller('HomeController', ['$scope','NewsService', function ($scope,NewsService) {
 
             $scope.message = "Welcome in our shop!!!";
 
+
+            /** Gets all the news. */
+            $scope.lstNews = NewsService.query();
+
+            /** Gets the news with id = 1. */
+            $scope.currentNews = NewsService.get({ id: 1 });
+            $scope.newsOfTheDay = NewsService.random();
+
+            /** Creates a new news. */
+            $scope.addedNews = new NewsService();
+
+            /** Increments the number of likes of a news, and saves the changes. */
+            $scope.addLike = function(news) {
+                news.$like();
+            };
+
+            /** Deletes a news. */
+            $scope.deleteNews = function(news) {
+                news.$delete(function(){
+                    $scope.lstNews.splice($scope.lstNews.indexOf(news),1);
+                });
+
+            };
+
+            /** Adds a news. */
+            $scope.addNews = function() {
+                $scope.addedNews.$save(function(){
+                    $scope.lstNews.push($scope.addedNews);
+                    $scope.addedNews = new NewsService();
+                });
+
+            };
         }]);
 })();
