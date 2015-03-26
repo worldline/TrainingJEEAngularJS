4c61153a850717d7dd61deed0d79a960fc9070b5
ffacon <francois.facon@gmail.com>
2015-03-27 00:40:31 +0100

    Update Detail controller in order to use productUtils service

--- src/main/webapp/bookcat/js/controllers/DetailController.js
+++ src/main/webapp/bookcat/js/controllers/DetailController.js
@@ -3,8 +3,8 @@
 
     /** Product detail view controller */
     angular.module('app')
-        .controller('DetailController', ['$scope', '$location', '$routeParams', 'catalogService',
-            function ($scope, $location, $routeParams, catalogService) {
+        .controller('DetailController', ['$scope', '$location', '$routeParams', 'catalogService', 'ProductUtils',
+            function ($scope, $location, $routeParams, catalogService, ProductUtils) {
 
             $scope.product = {};
 
@@ -13,21 +13,7 @@
             });
 
             $scope.quantity = 1;
-
-
-		    /** Returns rating for a given product. */
-		    var getProductRating = function(comments) {
-		        if (comments) {
-		            var sumRatings = 0;
-		            for ( var index = 0; index < comments.length; index++) {
-		                var comment = comments[index];
-		                sumRatings += comment.rate;
-		            }
-		            return (sumRatings / comments.length |0);  // like Math... but better :)
-		        }
-		        return 0;
-		    };
-		    
+	    
 		    $scope.getImage = function (id) {
 		        if (!id) {
 		            return "";
@@ -37,33 +23,7 @@
 		    };
 		    
 		    /** Returns the CSS class for the average rating of a given product. */
-		    $scope.getCSSRating = function(pItem) {
-		        var css = ['rating'];
-
-		        if (pItem !== undefined) {
-		            switch (getProductRating(pItem)) {
-		                case 1:
-		                    css.push('one');
-		                    break;
-		                case 2:
-		                    css.push('two');
-		                    break;
-		                case 3:
-		                    css.push('three');
-		                    break;
-		                case 4:
-		                    css.push('four');
-		                    break;
-		                case 5:
-		                    css.push('five');
-		                    break;
-		                default :
-		                    css.push('zero');
-		            }
-		        }
-		        return css;
-		    };
-
+		    $scope.getCSSRating = ProductUtils.getRatingCss;
 
 }]);
 })();
