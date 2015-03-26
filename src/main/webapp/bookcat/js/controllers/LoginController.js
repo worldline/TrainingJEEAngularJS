d4405cfe52999deb186e81b1e82d1726d80ad3c2
ffacon <francois.facon@gmail.com>
2015-03-26 09:11:40 +0100

    Use AuthenticationSharedService in LoginController

--- src/main/webapp/bookcat/js/controllers/LoginController.js
+++ src/main/webapp/bookcat/js/controllers/LoginController.js
@@ -1,10 +1,25 @@
 (function () {
     "use strict";
+    
+    angular.module('app').controller('LoginController', [ '$rootScope','$scope', '$location','AuthenticationSharedService',
+                                       function ( $rootScope,$scope, $location,AuthenticationSharedService)  {
+    	 								   $scope.errorMsg = null;	
+                                           $scope.logUser = function () {
+                                               AuthenticationSharedService.login({
+                                                   username: $scope.login,
+                                                   password: $scope.password,
+                                             
+                                               }).then(function () {
+                                                   $scope.authenticationError = false;
+                                                   $rootScope.authenticated = true;
+                                                   $location.path("/");
+                                               }).catch(function () {
+                                                   $scope.authenticationError = true;
+                                                   $rootScope.authenticated = false;
+                                               });
+                                           }
+                                       }]);
 
-    angular.module('app')
-        .controller('LoginController', ['$scope' , function ($scope) {
-
-        }]);
 })();
 
 
