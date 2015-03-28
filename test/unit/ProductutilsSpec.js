ca3bcdd617b9447d75d05c6bd87ffac0a6d544f8
ffacon <francois.facon@gmail.com>
2015-03-28 10:27:05 +0100

    Update ProductUtils unit test

--- test/unit/ProductutilsSpec.js
+++ test/unit/ProductutilsSpec.js
@@ -1,3 +1,27 @@
 describe("ProductUtils Service", function () {
-   
+	  var ProductUtils,
+      comments = [
+          {
+              "rate": 2,
+              "user": "Regis Legrand",
+              "comment": "Test comment"
+          },
+          {
+              "rate": 4,
+              "user": "François Facon",
+              "comment": "My comment"
+          }
+      ];
+
+	  beforeEach(function () {
+	
+	      module('app');
+	      inject(function ($injector) {
+	          ProductUtils = $injector.get('ProductUtils');
+	      });
+	  });
+	
+	  it("getRatingCss should return the correct CSS classes as an array", function () {
+	      expect(ProductUtils.getRatingCss(comments)).toEqual(['rating','three']);
+	  });
 });
