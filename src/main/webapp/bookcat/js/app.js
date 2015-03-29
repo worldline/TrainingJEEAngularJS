40f9038162fc75b4f1b8685ea79a5b1de6857799
ffacon <francois.facon@gmail.com>
2015-03-29 11:24:31 +0200

    Inject sdcoServices

--- src/main/webapp/bookcat/js/app.js
+++ src/main/webapp/bookcat/js/app.js
@@ -3,7 +3,7 @@ var baseUrl;
 (function () {
     "use strict";
 
-    var app = angular.module('app', [ 'ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'pascalprecht.translate']);
+    var app = angular.module('app', [ 'ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'pascalprecht.translate','sdcoServices']);
 
     /** Services configuration */
     app.config(
