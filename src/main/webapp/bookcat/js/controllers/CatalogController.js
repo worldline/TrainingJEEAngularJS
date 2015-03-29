824e09e2f4ee68c7207d67e146afa0250d30de5e
ffacon <francois.facon@gmail.com>
2015-03-29 11:25:44 +0200

    Update Catalog controller

--- src/main/webapp/bookcat/js/controllers/CatalogController.js
+++ src/main/webapp/bookcat/js/controllers/CatalogController.js
@@ -3,11 +3,22 @@
 
     /** Catalog view controller */
     angular.module('app')
-        .controller('CatalogController', ['$scope', '$location', 'catalogService', function ($scope, $location, catalogService) {
+        .controller('CatalogController', ['$scope', '$location', 'catalogService', 'ProductUtils', 'UserService', function ($scope, $location, catalogService, ProductUtils, UserService) {
 
             /** Returns all products. */
             catalogService.getCatalog().success(function (result) {
                 $scope.products = result;
             });
+            
+            $scope.getRatingClass = ProductUtils.getRatingCss;
+
+            $scope.getImage = ProductUtils.getImage;
+
+            $scope.addToCart = function(pItem){
+                 UserService.addToCart(pItem,1);
+                 $location.path('/basket');
+            };
+            
+            
         }]);
 })();
