fabd0b346ddfa45b7aeb80fb514501f2cf78b2a5
ffacon <francois.facon@gmail.com>
2015-03-29 11:26:58 +0200

    Update Basket controller

--- src/main/webapp/bookcat/js/controllers/BasketController.js
+++ src/main/webapp/bookcat/js/controllers/BasketController.js
@@ -3,9 +3,9 @@
 
     /** Basket view controller */
     angular.module('app')
-        .controller('BasketController', ['$scope', function ($scope) {
-            $scope.items = [];//todo
-            $scope.nbItem = 0; //todo
+        .controller('BasketController', ['$scope' , 'UserService', function ($scope, UserService) {
+            $scope.items = UserService.getCurrentUser().cart.getItems();
+            $scope.nbItem =  $scope.items.length;
             $scope.getTotal = function () {
                 var total = 0;
                 for(var index = 0 ; index < $scope.items.length ; index++){
