df54f18ffc7b8de6c14d94cefb22b41e67ba7afb
ffacon <francois.facon@gmail.com>
2015-03-30 09:56:12 +0200

    Update productSummary directive

--- src/main/webapp/bookcat/js/directives/productSummary.js
+++ src/main/webapp/bookcat/js/directives/productSummary.js
@@ -1,4 +1,29 @@
 (function(){
     "use strict";
 
+
+    angular.module('app').directive('productSummary', ['ProductUtils', function (ProductUtils) {
+
+        return {
+            restrict: 'E',
+            templateUrl: 'bookcat/templates/partials/productSummary.html',
+            scope:{
+                product:'='
+            },
+            link: function (scope, element, attrs) {
+                scope.getRatingClass = ProductUtils.getRatingCss;
+            },
+
+            controller: ['$scope','$location',
+                'UserService',
+                function ($scope,$location,UserService) {
+
+                    $scope.addToCart = function (pItem) {
+                        UserService.addToCart(pItem, 1);
+                         $location.path('/basket');
+                    };
+                }]
+        }
+
+    }]);
 })();
