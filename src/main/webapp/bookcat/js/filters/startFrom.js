7cbf76e683b7a10d7b58010f5c2bb729d81c37db
ffacon <francois.facon@gmail.com>
2015-03-31 11:51:22 +0200

    Update StartFrom Filter

--- src/main/webapp/bookcat/js/filters/startFrom.js
+++ src/main/webapp/bookcat/js/filters/startFrom.js
@@ -1,5 +1,15 @@
 (function(){
     "use strict";
     /** Filter used to return a collection which only elements at or after a given index. */
- 
+    angular.module('app')
+    .filter('startFrom', function() {
+        return function(pArray, pStartIndex) {
+            if (pArray !== undefined && pArray !== null) {
+                var filteredArray = [];
+                filteredArray = pArray.slice(pStartIndex);
+                return filteredArray;
+            }
+            return pArray;
+        };
+    });
 })();
